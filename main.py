329970d6ff5a15bbb7413f4446e0947f6bfcdb51
Alina Darmoroz <daarmorozaa@gmail.com>
2023-03-28 12:59:53 +0300

    Added compare method

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ def read_file(file_path):
         contents = file.readlines()
     return contents
 
+
+def compare_lines(lines1, lines2):
+    """Compares the content of two files and returns the matching and differing lines."""
+    same_lines = []
+    diff_lines = []
+    for line in lines1:
+        if line in lines2 and line not in same_lines:
+            same_lines.append(line)
+        elif line not in diff_lines:
+            diff_lines.append(line)
+    for line in lines2:
+        if line not in same_lines and line not in diff_lines:
+            diff_lines.append(line)
+    return same_lines, diff_lines
